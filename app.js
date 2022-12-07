98225bf0362d53d77bdb733d3ffde59af89f58b1
BaeGayeon <ebgy918@gmail.com>
2022-12-08 01:21:59 +0900

    test: lt test

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 var dotenv = require('dotenv');
-var sequelize = require('sequelize');
+// var sequelize = require('sequelize');
 
 const rosnodejs = require('rosnodejs');
 
@@ -15,11 +16,87 @@ function opnecv_capture() {
     client.call();
 }
 
+function self_door_open() {
+    //문 열어주세요. 성공적으로 열면 true 반환, 3초뒤에 item push해주세요
+    const client = nh.serviceClient('/opendoor2', 'std_srvs/Trigger');
+    client.call((resp) => {
+        console.log('Self door open Service response ' + JSON.stringify(resp));
+    });
+}
+function itempush(goal) {
+    //open에서 finish 받으면 실행 끝나면 true 반환, 5초 뒤에 self_door_close해주세요
+    const client = nh.serviceClient('/exection', 'std_srvs/Trigger');
+    client.call((resp) => {
+        console.log('itempush Service response ' + JSON.stringify(resp));
+    });
+}
+function self_door_close() {
+    //문 닫아주세요. 성공적으로 열면 true 반환
+    const client = nh.serviceClient('/closedoor2', 'std_srvs/Trigger');
+    client.call((resp) => {
+        console.log('self door close Service response ' + JSON.stringify(resp));
+    });
+}
+function human_door_open() {
+    //문 열어주세요. 성공적으로 열면 true 반환,
+    const client = nh.serviceClient('/opendoor1', 'std_srvs/Trigger');
+    client.call((resp) => {
+        console.log('Human door open Service response ' + JSON.stringify(resp));
+    });
+}
+
+function human_door_close() {
+    //문 닫아주세요. 성공적으로 열면 true 반환,
+    const client = nh.serviceClient('/closedoor1', 'std_srvs/Trigger');
+    client.call((resp) => {
+        console.log(
+            'human door close Service response ' + JSON.stringify(resp)
+        );
+    });
+}
+
+function destination(goal) {
+    //목적지 start 처음, middle 중간, final 끝
+    const client = nh.serviceClient('/closedoor', 'std_srvs/Trigger');
+    client.call({ dest: goal }, (resp) => {
+        console.log('Destination Service response ' + JSON.stringify(resp));
+    });
+}
+
+let middleArrive = rosNode.advertiseService(
+    '/middle_arrive',
+    SetBool,
+    (req, resp) => {
+        rosnodejs.log.info('Handling request! ' + JSON.stringify(req));
+        resp.success = !req.data;
+        resp.message = 'Inverted!';
+        return true;
+    }
+);
+
+const whitelist = [
+    'http://localhost:3000',
+    'https://eric-a.netlify.app',
+    'http://dev.bbbae.shop',
+];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        }
+    },
+    credentials: true,
+};
+
+function test() {
+    console.log('This is test!');
+}
+
 var app = express();
 
 app.get('/index.html', (req, res) => {
+    human_door_open();
     res.send('hello');
-    opencv_capture();
 });
 
 module.exports = app;
---
+++ services/delivery.js
@@ -0,0 +1,70 @@
+const { delivery, user } = require('../models');
+
+module.exports = {
+    findDelivery: async function () {
+        const deliveryResult = await delivery.findOne({
+            where: { status: '출발 전' },
+        });
+        const result = [];
+        result.push({
+            id: deliveryResult.dataValues.id,
+            name: deliveryResult.dataValues.name,
+            phoneNumber: deliveryResult.dataValues.phoneNumber,
+            destination: deliveryResult.dataValues.destination,
+            isInPerson: deliveryResult.dataValues.inPerson
+                ? '직접 수령하기'
+                : '두고 가기',
+            item: deliveryResult.dataValues.item,
+            status: deliveryResult.dataValues.status,
+            date: deliveryResult.dataValues.date,
+            time: deliveryResult.dataValues.time,
+            open: deliveryResult.dataValues.openRequest,
+        });
+        return result;
+    },
+    startDelivery: async function (id) {
+        delivery
+            .update(
+                {
+                    status: '접수 완료',
+                },
+                { where: { id: id } }
+            )
+            .then(() => {
+                return true;
+            })
+            .catch((err) => {
+                return false;
+            });
+    },
+    startCustomer: async function (id) {
+        delivery
+            .update(
+                {
+                    status: '배송 중',
+                },
+                { where: { id: id } }
+            )
+            .then(() => {
+                return true;
+            })
+            .catch((err) => {
+                return false;
+            });
+    },
+    arrive: async function (id) {
+        delivery
+            .update(
+                {
+                    status: '배송지 도착',
+                },
+                { where: { id: id } }
+            )
+            .then(() => {
+                return true;
+            })
+            .catch((err) => {
+                return false;
+            });
+    },
+};
